cd8d2f2be5fce821d324b8deeb8789239c0df167
Matthew Nichols <mattnichols@gmail.com>
2019-03-14 12:59:03 -0400

    Add rigid

--- src/features.cpp
+++ src/features.cpp
@@ -126,18 +126,25 @@ void find_alignment(cv::cuda::GpuMat& frame0, cv::cuda::GpuMat& frame1, cv::Mat&
       points_1.push_back( keypoints_1[ good[i].trainIdx ].pt );
     }
 
-  cv::Mat homo;
+  cv::Mat transform;
   if (good.size() > 10)
     {
 
-      homo = cv::findHomography( points_0, points_1, im_args.get("homo",args.get("homo",cv::RANSAC).asInt()).asInt() , im_args.get("ransac",args.get("ransac",5).asDouble()).asDouble());
-      //if (false)
-	if ( (homo.rows == 0) || (std::abs(1-homo.at<double>(0,0)) > 0.20) || (std::abs(1-homo.at<double>(1,1)) > 0.20))
+      if (im_args.get("transform", args.get("transform", "rigid").asString()).asString() == "affine")
 	{
-	  std::cout << "More than twenty percent variance in zoom or no homography found, this is probably an error, ignoring the transformation.\n";
+	  transform = cv::findTransformgraphy( points_0, points_1, im_args.get("homo",args.get("homo",cv::RANSAC).asInt()).asInt() , im_args.get("ransac",args.get("ransac",5).asDouble()).asDouble());
+	}
+      else
+	{
+	  transform = cv::estimateRigidTransform( points_0, points_1, false);
+	}
+      //if (false)
+      if ( (transform.rows == 0) || (std::abs(1-transform.at<double>(0,0)) > 0.20) || (std::abs(1-transform.at<double>(1,1)) > 0.20))
+	{
+	  std::cout << "More than twenty percent variance in zoom or no transformgraphy found, this is probably an error, ignoring the transformation.\n";
 	  if (debug)
 	    {
-	      std::cout << homo.at<double>(0,0) << ' ' << homo.at<double>(0,1) << ' ' << homo.at<double>(0,2) << "\n" << homo.at<double>(1,0) << ' ' << homo.at<double>(1,1) << ' ' << homo.at<double>(1,2) << "\n" << homo.at<double>(2,0) << ' ' << homo.at<double>(2,1) << ' ' << homo.at<double>(2,2) << "\n";
+	      std::cout << transform.at<double>(0,0) << ' ' << transform.at<double>(0,1) << ' ' << transform.at<double>(0,2) << "\n" << transform.at<double>(1,0) << ' ' << transform.at<double>(1,1) << ' ' << transform.at<double>(1,2) << "\n" << transform.at<double>(2,0) << ' ' << transform.at<double>(2,1) << ' ' << transform.at<double>(2,2) << "\n";
 	    }
 	  affine.at<float>(0,0) = 1.;
 	  affine.at<float>(0,1) = 0.;
@@ -150,9 +157,9 @@ void find_alignment(cv::cuda::GpuMat& frame0, cv::cuda::GpuMat& frame1, cv::Mat&
 	{
 	  if (debug)
 	    {
-	      std::cout << homo.at<double>(0,0) << ' ' << homo.at<double>(0,1) << ' ' << homo.at<double>(0,2) << "\n" << homo.at<double>(1,0) << ' ' << homo.at<double>(1,1) << ' ' << homo.at<double>(1,2) << "\n" << homo.at<double>(2,0) << ' ' << homo.at<double>(2,1) << ' ' << homo.at<double>(2,2) << "\n";
+	      std::cout << transform.at<double>(0,0) << ' ' << transform.at<double>(0,1) << ' ' << transform.at<double>(0,2) << "\n" << transform.at<double>(1,0) << ' ' << transform.at<double>(1,1) << ' ' << transform.at<double>(1,2) << "\n" << transform.at<double>(2,0) << ' ' << transform.at<double>(2,1) << ' ' << transform.at<double>(2,2) << "\n";
 	    }
-	  homo(cv::Range(0,2),cv::Range(0,3)).copyTo(affine);
+	  transform(cv::Range(0,2),cv::Range(0,3)).copyTo(affine);
 	  affine_found = true;
 	}
     }
